e7201f9bf6ec3e652bee9110d4cb4c44461cea61
Abreshmi Ahmed <92338736+CuteRosa16@users.noreply.github.com>
2025-04-11 14:57:03 +0100

    Updated user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,17 +1,3 @@
-/*
-  code explain : Added User Saved Posts Routes: Created Express routes to manage user saved posts. The /saved route retrieves a user's 
-      saved posts, while the /save route allows users to save or unsave posts. These routes connect to corresponding controller functions for handling database updates.
-    
-    reference: https://expressjs.com/en/guide/routing.html  
-
-
-    Made all the routes for user to make fetching and saving data in database
-  easy and efficiant.(routes: things we put after links- anime.com/solo-lvling , /solo-lvling is a route)
-
-  by using 2 diffrent route we use '/saved' to fetch multiple posts to show in our page, and
-    we use '/save' to see all the details of a single post.
-*/
-
 import express from "express"
 import { getUserSavedPosts, savePost } from "../controllers/user.controller.js"
 
