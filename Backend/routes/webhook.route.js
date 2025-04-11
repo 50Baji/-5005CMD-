f512d8be245fdcb3b967636d00b2d496b48f979c
Abreshmi Ahmed <92338736+CuteRosa16@users.noreply.github.com>
2025-04-11 14:57:18 +0100

    Updated webhook.route.js

--- Backend/routes/webhook.route.js
+++ Backend/routes/webhook.route.js
@@ -1,11 +1,3 @@
-/*
- reference: https://expressjs.com/en/guide/routing.html  
-
- code explain: Integrated Clerk Webhook Handling: Implemented an Express route to handle Clerk authentication webhooks. 
-      The /clerk route listens for incoming webhook events and processes them using the clerkWebHook controller. The request body is parsed as raw JSON using body-parser 
-      to ensure proper handling of webhook payloads.
-*/
-
 import express from "express";
 import { clerkWebHook } from "../controllers/webhook.controller.js";
 import bodyParser from "body-parser";
